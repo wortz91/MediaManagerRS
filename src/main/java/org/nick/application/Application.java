b4b1ed36d0399a4b0d3523a3bee60c7f335980d9
Nicholas Wortman <nick.osu27@gmail.com>
2016-06-05 15:39:07 -0400

    GenreDAO added.

--- src/main/java/org/nick/application/Application.java
+++ src/main/java/org/nick/application/Application.java
@@ -1,42 +1,21 @@
 package org.nick.application;
 
-import java.util.Date;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-import javax.persistence.Query;
+import org.nick.dao.GenreDAO;
 import org.nick.entities.Genre;
 
 public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("MediaManagerRS");
-		EntityManager em = emf.createEntityManager();
+		GenreDAO gDAO = new GenreDAO();
 		
-		EntityTransaction tx = em.getTransaction();
-		Genre gen = new Genre();
+		List<Genre> items = gDAO.getAllGenres();
 		
-		//NickTest is the value that is given by the user/website/whatever else
-		gen.setGenreDescription("NickTest " + new Date());
-		
-		tx.begin();
-		em.persist(gen);
-		tx.commit();
-		
-		Query query = em.createNamedQuery("Genre.findAll");
-		
-		List<Genre> list = query.getResultList();
-		
-		for(Genre genre : list) {
+		for(Genre genre : items) {
 			System.out.println(genre.getGenreDescription());
 		}
-		
-		em.close();
-		emf.close();
 	}
 
 }
